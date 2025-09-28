4b37686188126dff4855bb3cde5962c7146b0b31
bagheri <b.bagheri1234560@gmail.com>
2025-09-28 16:48:36 +0330

    Update visit card

--- 2.js
+++ 2.js
@@ -1,1 +1,17 @@
 //جاوا اسکریپت
+alert("به کارت ویزیت من خوش آمدید!");
+
+  // انتخاب عنوان
+  const title = document.querySelector(".front h1");
+  const card = document.querySelector(".card");
+
+  // وقتی موس رفت روی کارت
+  card.addEventListener("mouseenter", function () {
+    title.style.color = "blue"; // تغییر رنگ
+  });
+
+  // وقتی موس از کارت خارج شد
+  card.addEventListener("mouseleave", function () {
+    title.style.color = "rgb(107, 34, 175)"; // برگشت به رنگ اصلی
+  });
+
